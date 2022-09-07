3ddf6e5ff36d99487cb1718d112c02940d0f76ca
XFL <102812839+XFL310@users.noreply.github.com>
2022-09-07 19:32:54 +0800

    Add files via upload

---
+++ PurchasingMgmtSys/Models/ModifyInformationViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class ModifyInformationViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "New Email")]
+        public string NewEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Sex")]
+        public string Sex
+        {
+            get; set;
+        }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Confirm new Email")]
+        [Compare("NewEmail", ErrorMessage = "The new Email and confirmation Email do not match.")]
+        public string ConfirmEmail { get; set; }
+    }
+}
